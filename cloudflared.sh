c878066ffb4f96517096baefa9a8644c03fe1a6a
Tom <104158130+Ym0T@users.noreply.github.com>
2025-02-16 05:09:16 +0100

    Update cloudflared.sh
    Adjustments regarding the connection timeout and messages

--- cloudflared.sh
+++ cloudflared.sh
@@ -8,7 +8,7 @@ echo -e "\033[0;34m────────────────────�
 
 # [Tunnel] Check if cloudflared_token.txt exists and has content
 if [ -s "/home/container/cloudflared_token.txt" ]; then
-    echo -e "\033[0;37m[Tunnel] Starting Cloudflared with token...\033[0m"
+    echo -e "\033[0;37m[Tunnel] Starting Cloudflared with token\033[0m"
 
     # Start cloudflared in the background and store the PID
     cloudflared tunnel --no-autoupdate run --token "$(cat /home/container/cloudflared_token.txt)" > /home/container/logs/cloudflared.log 2>&1 &
@@ -16,28 +16,35 @@ if [ -s "/home/container/cloudflared_token.txt" ]; then
 
     echo $CLOUD_FLARED_PID > /home/container/tmp/cloudflared.pid
 
-    # Display waiting message
-    echo -ne "\033[0;33m[Tunnel] Waiting for Cloudflared to start\033[0m"
-
-    MAX_ATTEMPTS=10
+    MAX_ATTEMPTS=130
     ATTEMPT=0
+
+    # Times for status messages (seconds)
+    STATUS_TIMES=(5 10 15 30 60 90 120)
+
+    # Display waiting message
+    echo -e "\033[0;33m[Tunnel] Waiting for Cloudflared to start...\033[0m"
 
     # Monitor log file in real-time for a success or failure message
     while [ $ATTEMPT -lt $MAX_ATTEMPTS ]; do
-        echo -ne "."  # Add a dot every second
         sleep 1
         ATTEMPT=$((ATTEMPT + 1))
 
+        if [[ " ${STATUS_TIMES[@]} " =~ " $ATTEMPT " ]]; then
+            echo -e "\033[0;33m[Tunnel] Still waiting... ($ATTEMPT sec)\033[0m"
+        fi
+
         # Check if cloudflared has exited
         if ! kill -0 $CLOUD_FLARED_PID 2>/dev/null; then
-            echo -e "\n\033[0;31m[Tunnel] Cloudflared failed to start. Check logs at /home/container/logs/cloudflared.log\033[0m"
+            echo -e "\033[0;31m[Tunnel] Cloudflared failed to start. Check logs at /home/container/logs/cloudflared.log\033[0m"
             echo -e "\033[0;31m$(tail -n 10 /home/container/logs/cloudflared.log)\033[0m"
             exit 1
         fi
 
         # Check log file for success messages
         if grep -qE "Registered tunnel connection|Updated to new configuration" /home/container/logs/cloudflared.log; then
-            echo -e "\n\033[0;32m[Tunnel] Cloudflared is running successfully!\033[0m"
+        	echo -e "\033[0;32m[Tunnel] Connected after $ATTEMPT seconds\033[0m"
+            echo -e "\033[0;32m[Tunnel] Cloudflared is running successfully!\033[0m"
             exit 0
         fi
     done
